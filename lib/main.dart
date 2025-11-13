95e7fb55f742025369e1ef233cffbadc5589abc1
moustafawork9-cyber <moustafawork9@gmail.com>
2025-11-13 20:03:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: SimpleProject(),
+    );
+  }
+}
+
+
+class SimpleProject extends StatelessWidget {
+  const SimpleProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(elevation: 20,
+       title: Text("Facebook"), 
+       centerTitle: true,
+        leading: IconButton
+        (onPressed: (){},
+         icon: Icon
+         (Icons.menu, size: 30,
+         )),
+         actions: [
+           IconButton
+        (onPressed: (){},
+         icon: Icon
+         (Icons.message,
+          size: 26,
+         )),
+
+          IconButton
+        (onPressed: (){},
+         icon: Icon
+         (Icons.search,
+          size: 30,
+         )),],
+         ),
+
+      body: Center(
+        child: Text(
+          "Hello, Moustafa Abd Allatif 👋", 
+        style:TextStyle(fontSize: 25) 
+        ),
+      ),
+      )
+      ;
+  }
+}
